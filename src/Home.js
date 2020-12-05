e64400ca0839efdba53bf8d7fa7011b856977899
Raluca Rusu <rr.ralucarusu@gmail.com>
2020-12-05 13:50:08 +0200

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
@@ -29,215 +27,187 @@ class Home extends Component {
 	render() {
 		return (
 			<body>
-				<div className='is-boxed has-animations'>
-					<div className='body-wrap boxed-container'>
-						<main>
-							<div className='site-header-inner'>
-								<div className='brand header-brand'>
-									<Navbar>
-										<Navbar.Brand>
+			<div className='is-boxed has-animations'>
+				<div className='body-wrap boxed-container'>
+					<main>
+						<div className='site-header-inner'>
+							<div className='brand header-brand'>
+								<Navbar>
+									<Navbar.Brand>
+										<img
+											className='header-logo-image asset-light'
+											src='dist/images/logo-light.svg'
+											alt='Logo'></img>
+									</Navbar.Brand>
+								</Navbar>
+							</div>
+						</div>
+						<section className='hero'>
+							<div className='container'>
+								<div className='hero-inner'>
+									<div className='hero-copy'>
+										<h1
+											className='hero-title mt-0'
+											style={{ fontSize: '50px' }}>
+											Dating is now on videocall
+										</h1>
+										<br></br>
+										<p className='hero-paragraph'>
+											Now with dating while distancing on most singles’ minds,
+											doing a <b>VIDEO CALL DATE</b> before deciding to meet
+											someone is the new norm and this app is the place for
+											that.
+										</p>
+										<br></br>
+										<h4 className='hero-title mt-0'>
+											{' '}
+											The perfect date is only one click away from you and the
+											person you want to date{' '}
+										</h4>
+										<Button
+											class='button button-primary'
+											style={{ fontSize: '20px' }}
+											onClick={this.join}>
+											Go on the date already
+										</Button>
+										<br></br>
+										<br></br>
+										<br></br>
+									</div>
+									<div className='hero-media'>
+										<div className='header-illustration'>
 											<img
-												className='header-logo-image asset-light'
-												src='dist/images/logo-light.svg'
-												alt='Logo'></img>
-										</Navbar.Brand>
-									</Navbar>
+												className='header-illustration-image asset-light'
+												src='dist/images/header-illustration-light.svg'
+												alt='Header illustration'></img>
+										</div>
+										<div className='hero-media-illustration'>
+											<img
+												className='hero-media-illustration-image asset-light'
+												src='dist/images/hero-media-illustration-light.svg'
+												alt='Hero media illustration'></img>
+										</div>
+										<div className='hero-media-container'>
+											<img
+												className='hero-media-image asset-light'
+												src='dist/images/hero-media-light.svg'
+												alt='Hero media'></img>
+										</div>
+									</div>
 								</div>
 							</div>
-<<<<<<< HEAD
-							<div className="features-header text-center">
-								<br></br>
-								<h2 className="section-title">How does video call dating work?</h2>
-								<p className="section-paragraph">Lorem ipsum is common placeholder text used to
-									demonstrate the graphic elements of a document or visual presentation.</p>
-								<div className="features-image">
-									<img className="features-illustration asset-light"
-										 src="dist/images/features-illustration-light.svg"
-										 alt="Feature illustration"></img>
-									<img className="features-box asset-light"
-										 src="dist/images/features-box-light.svg" alt="Feature box"></img>
-									<img className="features-illustration asset-light"
-										 src="dist/images/features-illustration-top-light.svg"
-										 alt="Feature illustration top"></img>
-								</div>
-								<div className="features-header text-center">
-									<div class="features-wrap">
-										<div class="feature-inner">
-											<div class="feature-icon">
-												<img class="asset-light" src="dist/images/feature-01-light.svg" alt="Feature 01"></img>
-											</div>
-											<div class="feature-content">
-												<h3 class="feature-title mt-0">1. Press the button to start your date</h3>
-												<p class="text-sm mb-0">Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua dui.</p>
-											</div>
-=======
-							<section className='hero'>
-								<div className='container'>
-									<div className='hero-inner'>
-										<div className='hero-copy'>
-											<h1
-												className='hero-title mt-0'
-												style={{ fontSize: '50px' }}>
-												Dating is now on videocall
-											</h1>
-											<br></br>
-											<p className='hero-paragraph'>
-												Now with dating while distancing on most singles’ minds,
-												doing a <b>VIDEO CALL DATE</b> before deciding to meet
-												someone is the new norm and this app is the place for
-												that.
-											</p>
-											<br></br>
-											<h4 className='hero-title mt-0'>
-												{' '}
-												The perfect date is only one click away from you and the
-												person you want to date{' '}
-											</h4>
-											<Button
-												class='button button-primary'
-												style={{ fontSize: '20px' }}
-												onClick={this.join}>
-												Go on the date already
-											</Button>
-											<br></br>
-											<br></br>
-											<br></br>
->>>>>>> 154b8a8238471cbf6b027e4f108eef1ed4e90bd2
-										</div>
-										<div className='hero-media'>
-											<div className='header-illustration'>
-												<img
-													className='header-illustration-image asset-light'
-													src='dist/images/header-illustration-light.svg'
-													alt='Header illustration'></img>
-											</div>
-											<div className='hero-media-illustration'>
-												<img
-													className='hero-media-illustration-image asset-light'
-													src='dist/images/hero-media-illustration-light.svg'
-													alt='Hero media illustration'></img>
-											</div>
-											<div className='hero-media-container'>
-												<img
-													className='hero-media-image asset-light'
-													src='dist/images/hero-media-light.svg'
-													alt='Hero media'></img>
-											</div>
-										</div>
-									</div>
+							<div className='features-header text-center'>
+								<h2 className='section-title'>
+									How does video call dating work?
+								</h2>
+								<p className='section-paragraph'>
+									Lorem ipsum is common placeholder text used to demonstrate
+									the graphic elements of a document or visual presentation.
+								</p>
+								<div className='features-image'>
+									<img
+										className='features-illustration asset-light'
+										src='dist/images/features-illustration-light.svg'
+										alt='Feature illustration'></img>
+									<img
+										className='features-box asset-light'
+										src='dist/images/features-box-light.svg'
+										alt='Feature box'></img>
+									<img
+										className='features-illustration asset-light'
+										src='dist/images/features-illustration-top-light.svg'
+										alt='Feature illustration top'></img>
 								</div>
 								<div className='features-header text-center'>
-									<h2 className='section-title'>
-										How does video call dating work?
-									</h2>
-									<p className='section-paragraph'>
-										Lorem ipsum is common placeholder text used to demonstrate
-										the graphic elements of a document or visual presentation.
-									</p>
-									<div className='features-image'>
-										<img
-											className='features-illustration asset-light'
-											src='dist/images/features-illustration-light.svg'
-											alt='Feature illustration'></img>
-										<img
-											className='features-box asset-light'
-											src='dist/images/features-box-light.svg'
-											alt='Feature box'></img>
-										<img
-											className='features-illustration asset-light'
-											src='dist/images/features-illustration-top-light.svg'
-											alt='Feature illustration top'></img>
-									</div>
-									<div className='features-header text-center'>
-										<div class='features-wrap'>
-											<div class='feature-inner'>
-												<div class='feature-icon'>
-													<img
-														class='asset-light'
-														src='dist/images/feature-01-light.svg'
-														alt='Feature 01'></img>
-												</div>
-												<div class='feature-content'>
-													<h3 class='feature-title mt-0'>
-														1. Press the button to start your date
-													</h3>
-													<p class='text-sm mb-0'>
-														Lorem ipsum dolor sit amet, consecte adipiscing elit,
-														sed do eiusmod tempor incididunt ut labore et dolore
-														magna aliqua dui.
-													</p>
-												</div>
+									<div class='features-wrap'>
+										<div class='feature-inner'>
+											<div class='feature-icon'>
+												<img
+													class='asset-light'
+													src='dist/images/feature-01-light.svg'
+													alt='Feature 01'></img>
 											</div>
-											<br></br>
-											<div class='feature-inner'>
-												<div class='feature-icon'>
-													<img
-														class='asset-light'
-														src='dist/images/feature-02-light.svg'
-														alt='Feature 02'></img>
-												</div>
-												<div class='feature-content'>
-													<h3 class='feature-title mt-0'>
-														2. Write your name, accept using your mic & video
-													</h3>
-													<p class='text-sm mb-0'>
-														Lorem ipsum dolor sit amet, consecte adipiscing elit,
-														sed do eiusmod tempor incididunt ut labore et dolore
-														magna aliqua dui.
-													</p>
-												</div>
+											<div class='feature-content'>
+												<h3 class='feature-title mt-0'>
+													1. Press the button to start your date
+												</h3>
+												<p class='text-sm mb-0'>
+													Lorem ipsum dolor sit amet, consecte adipiscing elit,
+													sed do eiusmod tempor incididunt ut labore et dolore
+													magna aliqua dui.
+												</p>
 											</div>
-											<br></br>
-											<div class='feature-inner'>
-												<div class='feature-icon'>
-													<img
-														class='asset-light'
-														src='dist/images/feature-03-light.svg'
-														alt='Feature 03'></img>
-												</div>
-												<div class='feature-content'>
-													<h3 class='feature-title mt-0'>
-														3. Send the link to your date and start answering the
-														questions together
-													</h3>
-													<p class='text-sm mb-0'>
-														Lorem ipsum dolor sit amet, consecte adipiscing elit,
-														sed do eiusmod tempor incididunt ut labore et dolore
-														magna aliqua dui.
-													</p>
-												</div>
+										</div>
+										<br></br>
+										<div class='feature-inner'>
+											<div class='feature-icon'>
+												<img
+													class='asset-light'
+													src='dist/images/feature-02-light.svg'
+													alt='Feature 02'></img>
+											</div>
+											<div class='feature-content'>
+												<h3 class='feature-title mt-0'>
+													2. Write your name, accept using your mic & video
+												</h3>
+												<p class='text-sm mb-0'>
+													Lorem ipsum dolor sit amet, consecte adipiscing elit,
+													sed do eiusmod tempor incididunt ut labore et dolore
+													magna aliqua dui.
+												</p>
+											</div>
+										</div>
+										<br></br>
+										<div class='feature-inner'>
+											<div class='feature-icon'>
+												<img
+													class='asset-light'
+													src='dist/images/feature-03-light.svg'
+													alt='Feature 03'></img>
+											</div>
+											<div class='feature-content'>
+												<h3 class='feature-title mt-0'>
+													3. Send the link to your date and start answering the
+													questions together
+												</h3>
+												<p class='text-sm mb-0'>
+													Lorem ipsum dolor sit amet, consecte adipiscing elit,
+													sed do eiusmod tempor incididunt ut labore et dolore
+													magna aliqua dui.
+												</p>
 											</div>
 										</div>
 									</div>
 								</div>
-								<div></div>
-							</section>
-						</main>
+							</div>
+							<div></div>
+						</section>
+					</main>
 
-						<div style={{ backgroundColor: '#F0F0F0' }}>
-							<MDBFooter color='blue' className='font-small pt-4 mt-4'>
-								<MDBContainer fluid className='text-center text-md-left'>
-									<MDBRow>
-										<MDBCol md='3'></MDBCol>
-										<MDBCol md='6'>
-											<h5 className='title'>Footer Content</h5>
-											<p>
-												Here you can use rows and columns here to organize your
-												footer content.
-											</p>
-										</MDBCol>
-										<MDBCol md='3'></MDBCol>
-									</MDBRow>
+					<div style={{ backgroundColor: '#F0F0F0' }}>
+						<MDBFooter color='blue' className='font-small pt-4 mt-4'>
+							<MDBContainer fluid className='text-center text-md-left'>
+								<MDBRow>
+									<MDBCol md='3'></MDBCol>
+									<MDBCol md='6'>
+										<h5 className='title'>Footer Content</h5>
+										<p>
+											Here you can use rows and columns here to organize your
+											footer content.
+										</p>
+									</MDBCol>
+									<MDBCol md='3'></MDBCol>
+								</MDBRow>
+							</MDBContainer>
+							<div className='footer-copyright text-center py-3'>
+								<MDBContainer fluid>
+									&copy; {new Date().getFullYear()} Copyright
 								</MDBContainer>
-								<div className='footer-copyright text-center py-3'>
-									<MDBContainer fluid>
-										&copy; {new Date().getFullYear()} Copyright
-									</MDBContainer>
-								</div>
-							</MDBFooter>
-						</div>
+							</div>
+						</MDBFooter>
 					</div>
 				</div>
+			</div>
 			</body>
 		);
 	}
