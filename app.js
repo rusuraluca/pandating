7a70100a3593544bb853b1640752ef51cdea3942
Raluca Rusu <rr.ralucarusu@gmail.com>
2020-12-05 09:37:25 +0200

    heroku

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if(process.env.NODE_ENV==='production'){
 		res.sendFile(path.join(__dirname+"/build/index.html"))
 	})
 }
-app.set('port', (process.env.PORT || 4001))
+app.set('port', (process.env.PORT || 3001))
 
 sanitizeString = (str) => {
 	return xss(str)
--- src/Video.js
+++ src/Video.js
@@ -20,7 +20,8 @@ import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
 import "./Video.css"
 
-const server_url = process.env.NODE_ENV === 'production' ? 'https://video.sebastienbiollo.com' : "http://localhost:4001"
+//const server_url = process.env.NODE_ENV === 'production' ? 'https://pandating.herokuapp.com/' : "http://localhost:4001"
+const server_url = 'https://pandating.herokuapp.com/'
 
 var connections = {}
 const peerConnectionConfig = {
